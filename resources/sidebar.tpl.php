33922de5f4118f425c1d435c4046a6fc250d8af7
Ursula Clarke Everett <ursula@coderdojo.org>
2015-06-22 12:16:19 +0100

    Update sidebar link for Learning Resources

--- resources/sidebar.tpl.php
+++ resources/sidebar.tpl.php
@@ -8,8 +8,8 @@
             <a href="<?php echo $viewHelper["ArticlePath"], "Organising_a_Dojo"; ?>">Organiser Resources</a>
         </li>
 
-        <li id="TechnicalMenuItem" class="technical-resource dropdown<?php if($viewHelper['ThisPage'] == 'Overview_of_Learning_Resources'){ ?> active<?php } ?>">
-            <a href="<?php echo $viewHelper["ArticlePath"], "Overview_of_Learning_Resources"; ?>">Learning Resources</span></a>
+        <li id="TechnicalMenuItem" class="technical-resource dropdown<?php if($viewHelper['ThisPage'] == 'Learning_Resources'){ ?> active<?php } ?>">
+            <a href="<?php echo $viewHelper["ArticlePath"], "Learning_Resources"; ?>">Learning Resources</span></a>
         </li>
 		
         <li id="NinjaMenuItem" class="ninja-resource dropdown<?php if($viewHelper['ThisPage'] == 'The_Arcade'){ ?> active<?php } ?>">
